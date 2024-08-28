e86a2ac7e4eaa1eb97c4ae0016185d18187ea438
Riya Zingade <rzingade506@student.fuhsd.org>
2024-08-27 22:22:23 -0700

    Initial Commit

---
+++ mazah_api_2/mazah_api_2App.swift
@@ -0,0 +1,17 @@
+//
+//  mazah_api_2App.swift
+//  mazah_api_2
+//
+//  Created by Riya Zingade on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct mazah_api_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
